bf1465c52eaabd862f168fbdc37032b0f0948fa6
tian0912 <tianbin0912@163.com>
2021-02-27 23:52:22 +0800

    用户列表完成

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 import './plugins/element.js'
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+// axios请求拦截器统一注入token
+axios.interceptors.request.use(config=>{
+  //为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem("token")
+  return config
+})
+Vue.prototype.$http = axios
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,52 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Header,
+  Container,
+  Aside,
+  Main,
+  Menu,
+  MenuItem,
+  Submenu,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Tooltip,
+  Switch,
+  Pagination,
+  Dialog,
+  MessageBox
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Submenu)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Tooltip)
+Vue.use(Switch)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
